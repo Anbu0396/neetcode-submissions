5798d90440023b5f8af0757906a5203aba4f4fe5
Anbu <anbuselvan0396@gmail.com>
2026-07-24 00:36:08 +0530

    Add: binary-search - submission-4

---
+++ Data Structures & Algorithms/binary-search/submission-4.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return -1; // Handle empty array
+        }
+
+        int s = 0, e = nums.length - 1;
+
+        while (s <= e) {
+            int mid = s + (e - s) / 2; 
+
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                e = mid - 1; 
+            } else {
+                s = mid + 1; 
+            }
+        }
+
+        return -1; 
+    }
+}
