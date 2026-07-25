414d41c6435d1a59faa75fdcb5cd61f33b41a96e
Anbu <anbuselvan0396@gmail.com>
2026-07-25 23:33:38 +0530

    Add: length-of-last-word - submission-4

---
+++ Data Structures & Algorithms/length-of-last-word/submission-4.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        int i=s.length()-1;
+        while(i>=0 && s.charAt(i)==' ')
+        {
+            i--;
+        }
+        int a=0;
+        while(i>=0 && s.charAt(i)!=' ')
+        {
+            a++;
+            i--;
+        }
+        return a;
+    }
+}
